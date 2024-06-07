647448353d478080cfa5c67d5e94f980671f5234
ppwqydq <1643179928@qq.com>
2024-06-07 13:45:41 +0800

    最终通关版本

--- src/hust/cs/javacourse/search/query/impl/IndexSearcher.java
+++ src/hust/cs/javacourse/search/query/impl/IndexSearcher.java
@@ -3,7 +3,6 @@ package hust.cs.javacourse.search.query.impl;
 import hust.cs.javacourse.search.index.AbstractPosting;
 import hust.cs.javacourse.search.index.AbstractPostingList;
 import hust.cs.javacourse.search.index.AbstractTerm;
-import hust.cs.javacourse.search.index.impl.Posting;
 import hust.cs.javacourse.search.query.AbstractHit;
 import hust.cs.javacourse.search.query.AbstractIndexSearcher;
 import hust.cs.javacourse.search.query.Sort;
@@ -16,7 +15,6 @@ public class IndexSearcher extends AbstractIndexSearcher {
     @Override
     public void open(String indexFile) {
         index.load(new File(indexFile));
-        index.optimize();
     }
 
     /**
@@ -29,7 +27,7 @@ public class IndexSearcher extends AbstractIndexSearcher {
     @Override
     public AbstractHit[] search(AbstractTerm queryTerm, Sort sorter) {
         AbstractPostingList postingList= index.search(queryTerm);
-        if(postingList==null) return null;
+        if(postingList==null) return new AbstractHit[0];
         List<AbstractHit> hits= new ArrayList<>();
         for(int i=0; i<postingList.size(); i++){
             AbstractPosting posting= postingList.get(i);
@@ -56,70 +54,93 @@ public class IndexSearcher extends AbstractIndexSearcher {
     public AbstractHit[] search(AbstractTerm queryTerm1, AbstractTerm queryTerm2, Sort sorter, LogicalCombination combine) {
         AbstractPostingList postingList1 = index.search(queryTerm1);
         AbstractPostingList postingList2 = index.search(queryTerm2);
-        // 如果两个postingList都为null，则直接返回null
-        if (postingList1 == null && postingList2 == null) return null;
-        List<AbstractHit> hits = new ArrayList<>();
-        // 使用三元运算符来避免NullPointerException
-        int length1 = (postingList1 != null) ? postingList1.size() : 0;
-        int length2 = (postingList2 != null) ? postingList2.size() : 0;
+        if (postingList1 == null && postingList2 == null) return new AbstractHit[0];
 
-        int i = 0;
-        int j = 0;
-        // 创建一个空的Posting对象，其docId为-1，以便在比较时使用
-        Posting posting = new Posting(-1, -1, null);
-        while (i < length1 || j < length2) {
-            AbstractPosting posting1;
-            if ((i < length1)) {
-                assert postingList1 != null;
-                posting1 = postingList1.get(i);
-            } else {
-                posting1 = posting;
-            }
-            AbstractPosting posting2;
-            if ((j < length2)) {
-                assert postingList2 != null;
-                posting2 = postingList2.get(j);
-            } else {
-                posting2 = posting;
-            }
+        Map<Integer, Map<AbstractTerm, AbstractPosting>> result;
+        if (combine == LogicalCombination.AND) {
+            result = intersect(queryTerm1, queryTerm2, postingList1, postingList2);
+        } else {
+            result = union(queryTerm1, queryTerm2, postingList1, postingList2);
+        }
+        return getHits(sorter, result);
+    }
 
-            int docId1 = posting1.getDocId();
-            int docId2 = posting2.getDocId();
+    private AbstractHit[] getHits(Sort sorter, Map<Integer, Map<AbstractTerm, AbstractPosting>> result) {
+        if (result.isEmpty()) {
+            return new AbstractHit[0];
+        }
+        AbstractHit[] hits = new AbstractHit[result.size()];
+        List<Map.Entry<Integer, Map<AbstractTerm, AbstractPosting>>> entryList = new ArrayList<>(result.entrySet());
+        for (int i = 0; i < hits.length; i++) {
+            Map.Entry<Integer, Map<AbstractTerm, AbstractPosting>> entry = entryList.get(i);
+            hits[i] = new Hit(entry.getKey(), index.getDocName(entry.getKey()), entry.getValue());
+            hits[i].setScore(sorter.score(hits[i]));
+        }
+        List<AbstractHit> hitList = Arrays.asList(hits);
+        sorter.sort(hitList);
+        hits = hitList.toArray(hits);
+        return hits;
+    }
 
-            if(docId1==docId2){
-                String path1=index.getDocName(docId1);
-                HashMap<AbstractTerm,AbstractPosting> map=new HashMap<>();
-                map.put(queryTerm1, posting1);
-                map.put(queryTerm2, posting2);
-                AbstractHit hit=new Hit(docId1,path1,map);
-                hit.setScore(sorter.score(hit));
-                hits.add(hit);
+    private Map<Integer, Map<AbstractTerm, AbstractPosting>> intersect(AbstractTerm queryTerm1, AbstractTerm queryTerm2, AbstractPostingList postingList1, AbstractPostingList postingList2) {
+        Map<Integer, Map<AbstractTerm, AbstractPosting>> result = new HashMap<>();
+        int i = 0, j = 0;
+        while (i < postingList1.size() && j < postingList2.size()) {
+            AbstractPosting posting1 = postingList1.get(i);
+            AbstractPosting posting2 = postingList2.get(j);
+            if (posting1.getDocId() == posting2.getDocId()) {
+                Map<AbstractTerm, AbstractPosting> termPostingMapping = new HashMap<>();
+                termPostingMapping.put(queryTerm1, posting1);
+                termPostingMapping.put(queryTerm2, posting2);
+                result.put(posting1.getDocId(), termPostingMapping);
                 i++;
                 j++;
-            }
-            else if(docId1<docId2){
-                if(combine==LogicalCombination.OR){
-                    search(queryTerm1, sorter, hits, posting1, docId1);
-                }
+            } else if (posting1.getDocId() < posting2.getDocId()) {
                 i++;
-            }
-            else{
-                if(combine==LogicalCombination.AND){
-                    search(queryTerm2, sorter, hits, posting2, docId1);
-                }
+            } else {
                 j++;
             }
         }
-        sorter.sort(hits);
-        return hits.toArray(new AbstractHit[0]);
+        return result;
     }
 
-    private void search(AbstractTerm queryTerm, Sort sorter, List<AbstractHit> hits, AbstractPosting posting, int docId) {
-        String path=index.getDocName(docId);
-        HashMap<AbstractTerm,AbstractPosting> map=new HashMap<>();
-        map.put(queryTerm, posting);
-        AbstractHit hit=new Hit(docId,path,map);
-        hit.setScore(sorter.score(hit));
-        hits.add(hit);
+    private Map<Integer, Map<AbstractTerm, AbstractPosting>> union(AbstractTerm queryTerm1, AbstractTerm queryTerm2, AbstractPostingList postingList1, AbstractPostingList postingList2) {
+        Map<Integer, Map<AbstractTerm, AbstractPosting>> result = new HashMap<>();
+        int i = 0, j = 0;
+        while (i < postingList1.size() && j < postingList2.size()) {
+            AbstractPosting posting1 = postingList1.get(i);
+            AbstractPosting posting2 = postingList2.get(j);
+            if (posting1.getDocId() == posting2.getDocId()) {
+                Map<AbstractTerm, AbstractPosting> termPostingMapping = new HashMap<>();
+                termPostingMapping.put(queryTerm1, posting1);
+                termPostingMapping.put(queryTerm2, posting2);
+                result.put(posting1.getDocId(), termPostingMapping);
+                i++;
+                j++;
+            } else if (posting1.getDocId() < posting2.getDocId()) {
+                Map<AbstractTerm, AbstractPosting> termPostingMapping = new HashMap<>();
+                termPostingMapping.put(queryTerm1, posting1);
+                result.put(posting1.getDocId(), termPostingMapping);
+                i++;
+            } else {
+                Map<AbstractTerm, AbstractPosting> termPostingMapping = new HashMap<>();
+                termPostingMapping.put(queryTerm2, posting2);
+                result.put(posting2.getDocId(), termPostingMapping);
+                j++;
+            }
+        }
+        put(queryTerm1, postingList1, result, i);
+        put(queryTerm2, postingList2, result, j);
+        return result;
+    }
+
+    private void put(AbstractTerm queryTerm1, AbstractPostingList postingList1, Map<Integer, Map<AbstractTerm, AbstractPosting>> result, int i) {
+        while (i < postingList1.size()) {
+            AbstractPosting posting1 = postingList1.get(i);
+            Map<AbstractTerm, AbstractPosting> termPostingMapping = new HashMap<>();
+            termPostingMapping.put(queryTerm1, posting1);
+            result.put(posting1.getDocId(), termPostingMapping);
+            i++;
+        }
     }
 }
--- src/hust/cs/javacourse/search/run/TestBuildIndex.java
+++ src/hust/cs/javacourse/search/run/TestBuildIndex.java
@@ -1,5 +1,14 @@
 package hust.cs.javacourse.search.run;
 
+import hust.cs.javacourse.search.index.AbstractDocumentBuilder;
+import hust.cs.javacourse.search.index.AbstractIndex;
+import hust.cs.javacourse.search.index.AbstractIndexBuilder;
+import hust.cs.javacourse.search.index.impl.DocumentBuilder;
+import hust.cs.javacourse.search.index.impl.IndexBuilder;
+import hust.cs.javacourse.search.util.Config;
+
+import java.io.File;
+
 /**
  * 测试索引构建
  */
@@ -9,5 +18,6 @@ public class TestBuildIndex {
      * @param args : 命令行参数
      */
     public static void main(String[] args){
+
     }
 }
